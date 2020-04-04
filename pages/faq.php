51e67b8feee90a9ce6b9ebd3326b4c4cd49d4715
DanielImparatelu <danielimparatelu@gmail.com>
2020-04-04 17:13:27 +0100

    Add FAQ questions to resources.json

--- pages/faq.php
+++ pages/faq.php
@@ -1,4 +1,36 @@
 <?php include 'header.php' ?>
-<div > Test</div>
+
+<section class="faq-wrapper">
+    <div class = "faq-page-title">
+        <h3 class="subtitle-header align">
+            Frequently Asked Questions
+        </h3>
+    </div>
+    <div class="wrapper text-container container-fluid">
+        <div class="row">
+        
+                <div class="header-text-wrapper" style="width: 100%">
+                    <h3 class="subtitle-header align">
+                        How long does the application take?
+                    </h3>
+                </div>
+        
+
+        
+                <div class="paragraph-wrapper col-xs-12">
+                    <p class="sentence-text align">We are a Beauty Works supplier specialised in nano, micro, and tape hair extensions. We use the highest quality products to ensure proper fitting and maximum customer satisfaction</p>
+                    <p class="sentence-text align">Write some other shit here or something idk</p>
+                </div>
+        
+
+        
+                <div class="paragraph-wrapper col-xs-12">
+                    <p class="sentence-text align">Our customers are always satisfied with the quality and professionalism of the service, as well as the overall quality of the extensions fitted. </p>
+                    <!-- <p class="sentence-text align">Gallery</p> -->
+                </div>
+        
+        </div>
+    </div>
+</section>
 
 <?php include 'footer.php' ?>
